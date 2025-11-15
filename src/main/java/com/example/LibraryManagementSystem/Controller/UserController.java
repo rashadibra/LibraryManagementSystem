12fc88a784b7f0dd0967c8097acc936bc5df950c
Rashad Ibrahimli <rashad.ibra2003@gmail.com>
2025-11-15 23:19:05 +0400

    commit

--- src/main/java/com/example/LibraryManagementSystem/Controller/UserController.java
+++ src/main/java/com/example/LibraryManagementSystem/Controller/UserController.java
@@ -2,6 +2,9 @@ package com.example.LibraryManagementSystem.Controller;
 
 import com.example.LibraryManagementSystem.Entity.UserEntity;
 import com.example.LibraryManagementSystem.Service.UserService;
+import org.apache.catalina.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,13 +18,21 @@ import java.util.List;
 public class UserController {
     private UserService userService;
 
-    public UserController(UserService userService){
-        this.userService=userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
-//    Create User
+    //    Create User
     @PostMapping("/create_user")
-    public UserEntity createUser(@RequestBody UserEntity userEntity){
-      return userService.createUserService(userEntity);
+    public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity userEntity) {
+        try {
+            UserEntity createdUser = userService.createUserService(userEntity);
+            return ResponseEntity.ok(createdUser);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(null);
+        }
     }
+
+//
 }
